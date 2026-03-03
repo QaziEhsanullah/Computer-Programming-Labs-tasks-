00069ac4a7e4120887861b90fcebe758063d7fb4
QaziEhsanullah <azan19221@gmail.com>
2026-03-03 21:33:18 +0500

    Add files via upload

---
+++ CP LAB 3/LAB WORK/LAB 3 TASK 3.cpp
@@ -0,0 +1,23 @@
+// Name: Qazi Ehsanullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0738
+// Section: A
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+int num1; // VARIABLE DECLERE KRNY K LYE
+
+cout<<"enter num\n";// DISPLAY K LYE 
+cin>>num1;  // USER JO INPUT DE GA 
+
+if(num1>0)//   CONDITION LAGANY K LYE 
+cout<<num1<<" is positive"<<endl;   // PRINT KRNY K LYE 
+
+if(num1<0)//   CONDITION LAGAANY K LYE 
+cout<<num1<<" is negative"<<endl;//   DISPLAY KARNY K LYE 
+return 0;
+}
