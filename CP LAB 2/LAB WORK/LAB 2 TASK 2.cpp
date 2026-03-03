b96338e4c3125b4fb77d4dd58b8548d8ca390869
QaziEhsanullah <azan19221@gmail.com>
2026-03-03 21:31:27 +0500

    Add files via upload

---
+++ CP LAB 2/LAB WORK/LAB 2 TASK 2.cpp
@@ -0,0 +1,18 @@
+
+// Name: Qazi Ehsanullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0738
+// Section: A
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	float pi; // FLAOT USE KIA HA DECIMAL VALUE K LIYE
+    	pi=3.14; // VALUE DECLARE
+    	cout<<"value of pi =\t"<<pi; // DISPLAY
+
+
+return 0;
+}
---
+++ CP LAB 2/LAB WORK/LAB 2 TASK 3.cpp
@@ -0,0 +1,22 @@
+// Name: Qazi Ehsanullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0738
+// Section: A
+
+
+
+
+#include <iostream>
+using namespace std;
+int main()
+    {
+    	double F; // WORDS LIKHNY K LIYE
+    	float C; // DECIMAL VALUE K LIYE 
+    	cout<<"enter temperature in celcius ";  // DISPLAY
+    	cin>>C;
+    F = (C * 9/5) + 32;  // FORMULA CONVERRSION K LIYE
+    	cout<<F <<"  farheight";
+	
+
+return 0;
+}
