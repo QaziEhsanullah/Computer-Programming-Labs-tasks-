d4a79df8d251491ef6dc94dccfa882dc4edc437e
QaziEhsanullah <azan19221@gmail.com>
2026-03-03 21:24:08 +0500

    Add files via upload

---
+++ CP LAB 1/LAB WORK/LAB WORK (LAB 1).cpp
@@ -0,0 +1,17 @@
+// Name: Qazi Ehsanullah
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0738
+// Section: A
+
+
+
+#include <iostream>   // library ha iostream ma 
+using namespace std;  // standard name use krny k liay add kiay han
+
+int main() 
+{
+    
+    cout << "Hello World!"; // display k liye
+
+    return 0;  // program shuru b aur khatam b krta ha
+}
