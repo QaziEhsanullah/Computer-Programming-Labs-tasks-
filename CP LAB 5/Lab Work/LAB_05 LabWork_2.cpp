1328118a85fad3998c79e9a811c57f1d6152587c
QaziEhsanullah <azan19221@gmail.com>
2026-04-20 23:23:34 +0500

    Update LAB_05 LabWork_2.cpp

--- CP LAB 5/Lab Work/LAB_05 LabWork_2.cpp
+++ CP LAB 5/Lab Work/LAB_05 LabWork_2.cpp
@@ -2,19 +2,19 @@
 // Department: Electrical Engineering 
 // Registration No: BF25NWELE0738
 // Section: A
-#include <iostream>
-using namespace std;
+#include <iostream>  //input output
+using namespace std;  
 
-int main()
+int main() // main function body
 {
-    int i = 1;
+    int i = 1; /// initialization of variable
 
     do
     {
-        cout << i << endl;
+        cout << i << endl;  // uding do while loop
         i++;
     }
-    while(i <= 10);
+    while(i <= 10); //while loop for condition
 
     return 0;
 }
